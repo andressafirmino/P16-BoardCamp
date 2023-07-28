68f60be560f8b046debb11286c989cd864900adb
andressafirmino <andressafirmino78@gmail.com>
2023-07-28 16:27:52 -0300

    feat: PUT/customers

--- src/controllers/customers.controllers.js
+++ src/controllers/customers.controllers.js
@@ -8,10 +8,10 @@ export async function getCustomers(req, res) {
             const date = new Date(item.birthday);
             const formatDate = date.toISOString().split('T')[0];
             return {
-              ...item,
-              birthday: formatDate
+                ...item,
+                birthday: formatDate
             }
-          });          
+        });
         res.send(updatedData);
     } catch (e) {
         res.status(500).send(e.message);
@@ -19,21 +19,21 @@ export async function getCustomers(req, res) {
 }
 
 export async function getCustomersId(req, res) {
-    const {id} = req.params;
+    const { id } = req.params;
 
     try {
         const customer = await db.query(`SELECT * FROM customers WHERE id= $1;`, [id]);
-        if(customer.rows.length === 0) {
+        if (customer.rows.length === 0) {
             return res.status(404).send({ message: "Usuário não encontrado!" });
         }
         const updatedData = customer.rows.map(item => {
             const date = new Date(item.birthday);
             const formatDate = date.toISOString().split('T')[0];
             return {
-              ...item,
-              birthday: formatDate
+                ...item,
+                birthday: formatDate
             }
-          });     
+        });
         res.send(updatedData[0]);
     } catch (e) {
         res.status(500).send(e.message);
@@ -49,7 +49,6 @@ export async function postCustomers(req, res) {
             return res.status(409).send({ message: "Cliente já cadastrado!" });
         }
         await db.query(`INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4);`, [name, phone, cpf, birthday]);
-
         res.sendStatus(201);
     } catch (e) {
         res.status(500).send(e.message);
@@ -58,16 +57,19 @@ export async function postCustomers(req, res) {
 
 export async function putCustomers(req, res) {
     const { name, phone, cpf, birthday } = req.body;
-    const {id } = req.params;
+    const { id } = req.params;
 
     try {
-        const user = await db.query(`SELECT * FROM customers WHERE cpf = $1;`, [cpf]);
-        if (user.rows.length !== 0) {
+        const user = await db.query(`SELECT * FROM customers WHERE cpf = $1 ;`, [cpf]);
+        console.log(user.rows)
+        
+        if (user.rows.length !== 0 && user.rows[0].id !== parseInt(id)) {
             return res.status(409).send({ message: "Cliente já cadastrado!" });
         }
-        await db.query(`INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4);`, [name, phone, cpf, birthday]);
+        await db.query(`UPDATE customers SET name = $1, phone = $2, 
+        cpf = $3, birthday = $4 WHERE id = $5;`, [name, phone, cpf, birthday, parseInt(id)]);
 
-        res.sendStatus(201);
+        res.sendStatus(200);
     } catch (e) {
         res.status(500).send(e.message);
     }
