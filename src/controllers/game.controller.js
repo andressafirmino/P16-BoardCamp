0f28fbb67dcbab0b16bdbfe98ccba9df34aeeb58
andressafirmino <andressafirmino78@gmail.com>
2023-07-27 21:01:21 -0300

    feat: POST/games incompleto

--- src/controllers/game.controller.js
+++ src/controllers/game.controller.js
@@ -10,3 +10,19 @@ export async function getGame(req, res) {
         res.status(500).send(e.message);
     }
 }
+
+export async function postGame(req, res) {
+    const { name, image, stockTotal, pricePerDay } = req.body;
+
+    try {
+        const game = await db.query(`SELECT * FROM games WHERE name = $1;`, [name]);
+        if (game.rows.length !== 0) {
+            return res.status(409).send({ message: "Jogo já cadastrado!" });
+        }
+        await db.query(`INSERT INTO games (name, image, stockTotal, pricePerDay) VALUES ($1, $2, $3, $4);`, [name, image, stockTotal, pricePerDay]);
+
+        res.sendStatus(201);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
+}
--- src/routes/game.routes.js
+++ src/routes/game.routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { getGame } from "../controllers/game.controller.js";
+import { getGame, postGame } from "../controllers/game.controller.js";
+import { validateSchema } from "../middlewares/validateSchema.js";
+import { postGameSchema } from "../schemas/game.schema.js";
 
 const gameRouter = Router();
 
 gameRouter.get("/games", getGame);
+gameRouter.post("/games", validateSchema(postGameSchema), postGame);
 
 export default gameRouter;
