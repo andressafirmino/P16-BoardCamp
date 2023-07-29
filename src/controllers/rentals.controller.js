d96405d25d3cf94231f0a46edfd3c96104aa7b38
andressafirmino <andressafirmino78@gmail.com>
2023-07-28 22:05:14 -0300

    feat: validação stockTotal

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -5,17 +5,19 @@ export async function getRentals(req, res) {
 }
 
 export async function postRentals(req, res) {
-    const {customerId, gameId, daysRented} = req.body;
+    const { customerId, gameId, daysRented } = req.body;
 
     try {
         const customer = await db.query(`SELECT * FROM customers WHERE id= $1;`, [customerId]);
         const game = await db.query(`SELECT * FROM games WHERE id= $1;`, [gameId]);
-        if (customer.rows.length === 0 || game.rows.length === 0) {
+        const rental = await db.query(`SELECT * FROM rentals WHERE "gameId"= $1;`, [gameId]);
+        
+        if (customer.rows.length === 0 || game.rows.length === 0 || game.rows[0].stockTotal <= rental.rows.length) {
             return res.sendStatus(400);
         }
         const price = await db.query(`SELECT ("pricePerDay") FROM games WHERE id = $1;`, [gameId]);
         await db.query(`INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee") 
-        VALUES ($1, $2, $3, $4, $5, $6, $7);`, [customerId, gameId, new Date(), daysRented, null, daysRented*price.rows[0].pricePerDay, null]);
+        VALUES ($1, $2, $3, $4, $5, $6, $7);`, [customerId, gameId, new Date(), daysRented, null, daysRented * game.rows[0].pricePerDay, null]);
         res.sendStatus(201);
     } catch (e) {
         res.status(500).send(e.message);
