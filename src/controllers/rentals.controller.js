c6f89876500c6ac300fb18186ad5652af605eb11
andressafirmino <andressafirmino78@gmail.com>
2023-07-28 21:43:22 -0300

    refactor: calculo originalPrice

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -15,7 +15,7 @@ export async function postRentals(req, res) {
         }
         const price = await db.query(`SELECT ("pricePerDay") FROM games WHERE id = $1;`, [gameId]);
         await db.query(`INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee") 
-        VALUES ($1, $2, $3, $4, $5, $6, $7);`, [customerId, gameId, new Date(), daysRented, null, 3*price.rows[0].pricePerDay, null]);
+        VALUES ($1, $2, $3, $4, $5, $6, $7);`, [customerId, gameId, new Date(), daysRented, null, daysRented*price.rows[0].pricePerDay, null]);
         res.sendStatus(201);
     } catch (e) {
         res.status(500).send(e.message);
