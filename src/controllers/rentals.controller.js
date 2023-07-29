40bf34f566d1ef72eefd44bd5ce011308dd7f441
andressafirmino <andressafirmino78@gmail.com>
2023-07-28 21:34:20 -0300

    feat: adicionar dados na tabela rentals

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -6,8 +6,11 @@ export async function getRentals(req, res) {
 
 export async function postRentals(req, res) {
     const {customerId, gameId, daysRented} = req.body;
-    try {
 
+    try {
+        const price = await db.query(`SELECT ("pricePerDay") FROM games WHERE id = $1;`, [gameId]);
+        await db.query(`INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee") 
+        VALUES ($1, $2, $3, $4, $5, $6, $7);`, [customerId, gameId, new Date(), daysRented, null, 3*price.rows[0].pricePerDay, null]);
         res.sendStatus(201);
     } catch (e) {
         res.status(500).send(e.message);
