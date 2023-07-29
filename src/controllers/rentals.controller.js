0d732d5e28e2c3aa9ffa1d014f03c124cc531328
andressafirmino <andressafirmino78@gmail.com>
2023-07-28 21:39:00 -0300

    feat: validação do customerId

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -8,6 +8,10 @@ export async function postRentals(req, res) {
     const {customerId, gameId, daysRented} = req.body;
 
     try {
+        const customer = await db.query(`SELECT * FROM customers WHERE id= $1;`, [customerId]);
+        if (customer.rows.length === 0) {
+            return res.sendStatus(400);
+        }
         const price = await db.query(`SELECT ("pricePerDay") FROM games WHERE id = $1;`, [gameId]);
         await db.query(`INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee") 
         VALUES ($1, $2, $3, $4, $5, $6, $7);`, [customerId, gameId, new Date(), daysRented, null, 3*price.rows[0].pricePerDay, null]);
